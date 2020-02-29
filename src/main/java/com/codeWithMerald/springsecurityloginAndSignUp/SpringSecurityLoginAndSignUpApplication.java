f6f955e14088bb46dba338d76dbdf90b63d767a1
Meralds-World <grace.chidumaga@gmail.com>
2020-02-29 17:22:48 +0100

    first commit

---
+++ src/main/java/com/codeWithMerald/springsecurityloginAndSignUp/SpringSecurityLoginAndSignUpApplication.java
@@ -0,0 +1,13 @@
+package com.codeWithMerald.springsecurityloginAndSignUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLoginAndSignUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLoginAndSignUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeWithMerald/springsecurityloginAndSignUp/SpringSecurityLoginAndSignUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeWithMerald.springsecurityloginAndSignUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLoginAndSignUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
